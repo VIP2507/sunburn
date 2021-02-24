8eb37bd36db7d95e725548b316e998682bc54708
prince chauhan <viprajput2507@gmail.com>
2021-02-23 20:52:05 -0800

    Updates

---
+++ precache-manifest.8f2fe114831c818888b00b9a5df92c6c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60f12376d641a0e166470adb94e060dc",
+    "url": "/sunburn/index.html"
+  },
+  {
+    "revision": "4bb4f6caab77610c19be",
+    "url": "/sunburn/static/css/main.13b961e3.chunk.css"
+  },
+  {
+    "revision": "b566ab7a04cd354792b6",
+    "url": "/sunburn/static/js/2.f624984f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/sunburn/static/js/2.f624984f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bb4f6caab77610c19be",
+    "url": "/sunburn/static/js/main.0631c0d1.chunk.js"
+  },
+  {
+    "revision": "54bb201b5dbf5b6135c6",
+    "url": "/sunburn/static/js/runtime-main.35251d9c.js"
+  },
+  {
+    "revision": "bd1392bc154b1368c1b07b1f8dcfdec1",
+    "url": "/sunburn/static/media/slide_five.bd1392bc.jpg"
+  },
+  {
+    "revision": "80092545ff4c8b91c3dbe23ca41c0515",
+    "url": "/sunburn/static/media/slide_four.80092545.jpg"
+  },
+  {
+    "revision": "073d545eec7487ac2cbb8c0d0e5b955a",
+    "url": "/sunburn/static/media/slide_one.073d545e.jpg"
+  },
+  {
+    "revision": "0ce7e536ae7d15b5cbf87535f37e633e",
+    "url": "/sunburn/static/media/slide_three.0ce7e536.jpg"
+  },
+  {
+    "revision": "d1672d55f6eb36952a237d4f98cad696",
+    "url": "/sunburn/static/media/sun6.d1672d55.jpg"
+  },
+  {
+    "revision": "78103f1d83613fa54499b52959652973",
+    "url": "/sunburn/static/media/sun7.78103f1d.jpg"
+  }
+]);
